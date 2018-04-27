dabbad60fc2f2c05f630bbad7a27a56ad6a83f53
Michael Kornblum <mkornblum71@gmail.com>
2018-04-27 14:07:54 -0500

    it now works

--- gulp_tasks/image-resize.js
+++ gulp_tasks/image-resize.js
@@ -7,29 +7,34 @@ const {
 } = require('gulp-load-plugins')();
 
 module.exports = () =>
-  src('images/resized/*')
+  src('./images/resized/**/*')
     .pipe(plumber())
     .pipe(responsiveImages({
       '*.jpg': [
         {
           width: 1920,
           suffix: '-lg',
+          overwrite: true,
         },
         {
           width: 960,
           suffix: '-med',
+          overwrite: true,
         },
         {
           width: 480,
           suffix: '-sm',
+          overwrite: true,
         },
         {
           width: 480,
           suffix: '-lofi',
+          quality: 20,
+          overwrite: true,
         },
       ],
     }))
     .pipe(smushit({
       verbose: true,
     }))
-    .pipe(dest('build/images'));
+    .pipe(dest('./build/images/'));
