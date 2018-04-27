765cd32d8e274860681c161bfed2f46c321ff50e
Michael Kornblum <mkornblum71@gmail.com>
2018-04-27 13:20:39 -0500

    initial commit

---
+++ gulp_tasks/image-resize.js
@@ -0,0 +1,35 @@
+import { src, dest } from 'gulp';
+
+const {
+  plumber,
+  responsiveImages,
+  smushit,
+} = require('gulp-load-plugins')();
+
+module.exports = () =>
+  src('images/resized/*')
+    .pipe(plumber())
+    .pipe(responsiveImages({
+      '*.jpg': [
+        {
+          width: 1920,
+          suffix: '-lg',
+        },
+        {
+          width: 960,
+          suffix: '-med',
+        },
+        {
+          width: 480,
+          suffix: '-sm',
+        },
+        {
+          width: 480,
+          suffix: '-lofi',
+        },
+      ],
+    }))
+    .pipe(smushit({
+      verbose: true,
+    }))
+    .pipe(dest('build/images'));
