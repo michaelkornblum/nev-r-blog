461765b9dab4e67720b6114ac8524f61c1a16f6a
Michael Kornblum <mkornblum71@gmail.com>
2018-04-28 01:45:27 -0500

    add image compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
-const { task } = require('gulp');
-const { imageResize } = require('./gulp_tasks');
+const { task, series } = require('gulp');
+const { imageResize, imageCompress } = require('./gulp_tasks');
 
 task('image:resize', imageResize);
+task('image:compress', imageCompress);
+
+task('images', series('image:compress', 'image:resize'));
