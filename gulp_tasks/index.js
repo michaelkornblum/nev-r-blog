8417b6a92bd824ed13e1df14c49e00a25961015e
Michael Kornblum <mkornblum71@gmail.com>
2018-04-28 02:04:24 -0500

    move lower level gulp tasks to gulp_tasks index

--- gulp_tasks/index.js
+++ gulp_tasks/index.js
@@ -1,2 +1,5 @@
-exports.imageResize = require('./image-resize');
-exports.imageCompress = require('./image-compress');
+const { task } = require('gulp');
+
+task('image:resize', require('./image-resize'));
+task('image:compress', require('./image-compress'));
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,4 @@
 const { task, series } = require('gulp');
-const { imageResize, imageCompress } = require('./gulp_tasks');
+require('./gulp_tasks');
 
-task('image:resize', imageResize);
-task('image:compress', imageCompress);
-
-task('images', series('image:compress', 'image:resize'));
+task('images', series('image:resize', 'image:compress'));
