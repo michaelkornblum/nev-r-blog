4142c38b00f2857023063c504c93dd7a74e3cd97
Michael Kornblum <mkornblum71@gmail.com>
2018-04-28 03:29:57 -0500

    remove responsive-image plugin

--- gulp_tasks/image-compress.js
+++ gulp_tasks/image-compress.js
@@ -1,26 +1,9 @@
 const { src, dest } = require('gulp');
-
-const {
-  plumber,
-  responsiveImages,
-  smushit,
-} = require('gulp-load-plugins')();
+const config = require('../configs/gulp.config').imageCompress;
+const { plumber, smushit } = require('gulp-load-plugins')();
 
 module.exports = () =>
-  src('./images/cropped/**/*')
+  src(config.src)
     .pipe(plumber())
-    .pipe(responsiveImages({
-      '*.jpg': [
-        {
-          suffix: '-lofi',
-          quality: 25,
-        },
-        {
-          quality: 100,
-        },
-      ],
-    }))
-    .pipe(smushit({
-      verbose: true,
-    }))
-    .pipe(dest('./build/images/'));
+    .pipe(smushit(config.smushit))
+    .pipe(dest(config.dest));
