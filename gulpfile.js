d6738c2efd2a5ab7d0e0ea364eea004347b7add2
Michael Kornblum <mkornblum71@gmail.com>
2018-04-29 13:46:51 -0500

    add stylus compilation

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ const {
   imageResize,
   imageCompress,
   javascriptBundle,
+  stylusCompile,
 } = require('./gulp_tasks');
 
 // base tasks
 task('image:resize', imageResize);
 task('image:compress', imageCompress);
 task('js:bundle', javascriptBundle);
+task('stylus:compile', stylusCompile);
 
 
 task('images', series('image:resize', 'image:compress'));
