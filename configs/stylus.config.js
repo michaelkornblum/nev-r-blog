56b8426aa4cbca7eec66e9188dad414e40bf2687
Michael Kornblum <mkornblum71@gmail.com>
2018-04-30 16:30:16 -0500

    initial commit

--- configs/stylus.config.js
+++ configs/stylus.config.js
@@ -1,10 +1,8 @@
-const typographic = require('typographic');
 const rupture = require('rupture');
 const postStylus = require('poststylus');
 const rucksack = require('rucksack-css');
 const lost = require('lost');
-const autoprefixer = require('autoprefixer');
-const csswring = require('csswring');
+const typographic = require('typographic');
 
 module.exports = {
   use: [
@@ -13,8 +11,6 @@ module.exports = {
     postStylus([
       rucksack,
       lost,
-      autoprefixer({ browsers: 'last 2 versions' }),
-      csswring,
     ]),
   ],
 };
