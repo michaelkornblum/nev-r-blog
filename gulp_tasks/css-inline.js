e91436c0aa74df297b4b24dcffde38d2da80fa77
Michael Kornblum <mkornblum71@gmail.com>
2018-05-01 01:35:45 -0500

    initial commit

--- gulp_tasks/css-inline.js
+++ gulp_tasks/css-inline.js
@@ -2,13 +2,9 @@ const { src, dest } = require('gulp');
 const plumber = require('gulp-plumber');
 const critical = require('critical').stream;
 const config = require('../configs/gulp.config').criticalCSS;
-const log = require('fancy-log');
 
 module.exports = () =>
   src(config.src)
     .pipe(plumber())
     .pipe(critical(config.critical))
-    .on('error', (err) => {
-      log.err(err.message);
-    })
     .pipe(dest(config.dest));
