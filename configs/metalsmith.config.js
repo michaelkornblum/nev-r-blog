3c38f533669fe20287f3deb6620891530c02ed4f
Michael Kornblum <mkornblum71@gmail.com>
2018-05-12 11:57:35 -0500

    add remaining metalsmith plugins

--- configs/metalsmith.config.js
+++ configs/metalsmith.config.js
@@ -1,8 +1,14 @@
 const {
   markdown,
+  excerpts,
   collections,
   permalinks,
+  pagination,
   layouts,
+  tags,
+  metallic,
+  feed,
+  domTransform,
 } = require('load-metalsmith-plugins')();
 
 const config = require('./site.config');
@@ -11,6 +17,9 @@ module.exports = {
   metadata: config,
   use: [
     markdown(),
+    domTransform(),
+    excerpts(),
+    metallic(),
     collections({
       posts: {
         pattern: 'posts/**.html',
@@ -20,6 +29,27 @@ module.exports = {
       pages: {
         pattern: 'pages/**.html',
       },
+    }),
+    pagination({
+      'collections.posts': {
+        perPage: 5,
+        layout: 'archives.pug',
+        first: 'index.html',
+        path: 'archives/:num/index.html',
+        pageMetadata: {
+          title: 'Archive',
+        },
+      },
+    }),
+    feed({ collection: 'posts' }),
+    tags({
+      handle: 'tags',
+      path: 'tags/:tag.html',
+      layout: 'tag.pug',
+      sortBy: 'date',
+      reverse: true,
+      metadataKey: 'category',
+      slug: { mode: 'rfc3986' },
     }),
     permalinks({
       relative: false,
--- src/posts/first-post.md
+++ src/posts/first-post.md
@@ -3,6 +3,7 @@ title: First Post
 author: Michael Kornblum
 date: 2018-05-10
 collection: posts
+tags: life, the universe, everything
 ---
 
 This is my first post. Yay!
